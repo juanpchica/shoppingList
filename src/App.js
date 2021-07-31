646b56e3d5c074c8d519d9fa5d0986228b4dcd43
juanpchica <juanpchica@hotmail.com>
2021-07-31 14:16:18 -0500

    Configuring menu and react router

--- src/App.js
+++ src/App.js
@@ -5,14 +5,24 @@ import "./App.css";
 
 //External Components
 import Container from "react-bootstrap/Container";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 //Own components
 import { NavbarMenu } from "./components/Navbar";
+import { Todo } from "./pages/Todo";
+import { Api } from "./pages/Api";
 function App() {
   return (
     <React.Fragment>
-      <NavbarMenu />
-      <Container></Container>
+      <Router>
+        <NavbarMenu />
+        <Container>
+          <Switch>
+            <Route path='/todo' component={Todo} />
+            <Route path='/api' component={Api} />
+          </Switch>
+        </Container>
+      </Router>
     </React.Fragment>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,15 +5,20 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 
+import { Link } from "react-router-dom";
+
 export const NavbarMenu = () => {
   return (
     <Navbar bg='dark' variant='dark'>
       <Container>
         <Navbar.Brand href='#home'>JP</Navbar.Brand>
         <Nav className='me-auto'>
-          <Nav.Link href='#home'>TODO</Nav.Link>
-          <Nav.Link href='#features'>API</Nav.Link>
-          <Nav.Link href='#pricing'>Pricing</Nav.Link>
+          <Nav.Link as={Link} to='/todo'>
+            TODO
+          </Nav.Link>
+          <Nav.Link as={Link} to='/api'>
+            API
+          </Nav.Link>
         </Nav>
       </Container>
     </Navbar>
