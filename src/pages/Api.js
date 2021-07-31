a9e95e5430a9e5688fe6eb7b64f77999761672de
juanpchica <juanpchica@hotmail.com>
2021-07-31 16:51:36 -0500

    Initializing api component

--- src/pages/Api.js
+++ src/pages/Api.js
@@ -1,5 +1,22 @@
 import React from "react";
 
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import ListGroup from "react-bootstrap/ListGroup";
+
 export const Api = () => {
-  return <div>api</div>;
+  return (
+    <Container>
+      <Row>
+        <Col xs={4}>
+          <ListGroup>
+            <ListGroup.Item>Cras justo odio</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>
+  );
 };
