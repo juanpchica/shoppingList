16ce45009de7a8120b83acb47b6a458579a18287
juanpchica <juanpchica@hotmail.com>
2021-07-31 21:53:49 -0500

    Reducer account modified

--- src/state/reducers/accountReducer.js
+++ src/state/reducers/accountReducer.js
@@ -1,1 +1,12 @@
-const reducer = (state, action) => {};
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case "deposit":
+      return state + action.payload;
+    case "withdraw":
+      return state - action.payload;
+    default:
+      return state;
+  }
+};
+
+export default reducer;
