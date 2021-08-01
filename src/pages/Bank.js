1affb02d13c02a757cc0046c4fbe85cd3b28e485
juanpchica <juanpchica@hotmail.com>
2021-07-31 22:45:45 -0500

    Adding UI for bank component

--- src/pages/Bank.js
+++ src/pages/Bank.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { useSelector } from "react-redux";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export const Bank = () => {
-  const state = useSelector((state) => state);
-  console.log(state);
-  return <div>Bank</div>;
+  const [valueAccount, setValueAccount] = useState("");
+
+  const accountValue = useSelector((state) => state.account);
+  return (
+    <Container style={{ padding: 50 }}>
+      <Card>
+        <Card.Body>{`Total account $${accountValue}`}</Card.Body>
+      </Card>
+
+      <Card>
+        <Card.Body>
+          <Form.Control
+            type='text'
+            placeholder='Enter Value:'
+            name='valueAccount'
+            value={valueAccount}
+            onChange={(e) => {
+              setValueAccount(e.targe.value);
+            }}
+          />
+          <Button variant='primary' className='btn-space'>
+            Deposit
+          </Button>
+          <Button variant='secondary' className='btn-space'>
+            Withdraw
+          </Button>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
 };
--- src/state/store.js
+++ src/state/store.js
@@ -1,4 +1,8 @@
 import { createStore } from "redux";
 import reducers from "./reducers/index";
 
-export const store = createStore(reducers, {});
+export const store = createStore(
+  reducers,
+  /* preloadedState, */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
