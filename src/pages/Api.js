9f6026af3ad0c20b33ccbc306e661f4a8a9850e3
juanpchica <juanpchica@hotmail.com>
2021-08-08 10:56:15 -0500

    Listing years from click event

--- src/pages/Api.js
+++ src/pages/Api.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
@@ -13,7 +13,8 @@ const API_URL = "https://jsonmock.hackerrank.com/api/football_competitions";
 export const Api = () => {
   const [dataMenu, setDataMenu] = useState([]);
   const [years, setYears] = useState([]);
-
+  const [year, setYear] = useState({});
+  const [actualYear, setActualYear] = useState([]);
   const getYears = (data) => {
     const newDataYears = [
       ...new Set(
@@ -31,12 +32,22 @@ export const Api = () => {
       const res = await axios.get(API_URL);
       if (res && res.data) {
         setDataMenu(res.data);
+        setYear(res.data.data[0]);
         getYears(res.data.data);
       }
     };
 
     getMenuData();
   }, []);
+
+  const showYear = (year) => {
+    axios
+      .get(`${API_URL}?year=${year}`)
+      .then(({ data: { data: championships } }) => {
+        setActualYear(championships);
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <Container>
@@ -45,14 +56,45 @@ export const Api = () => {
           <ListGroup>
             {years && years.length > 0 ? (
               years.map((year, index) => {
-                return <ListGroup.Item key={index}>{year}</ListGroup.Item>;
+                return (
+                  <ListGroup.Item
+                    key={index}
+                    onClick={() => {
+                      showYear(year);
+                    }}
+                  >
+                    {year}
+                  </ListGroup.Item>
+                );
               })
             ) : (
               <h3>Not data found...</h3>
             )}
           </ListGroup>
         </Col>
-        <Col></Col>
+        <Col>
+          {actualYear.length > 0 ? (
+            <ListGroup>
+              {actualYear.map((year, index) => {
+                return (
+                  <ListGroup.Item key={index}>
+                    <Fragment>
+                      <h2>{year.name}</h2>
+                      <span>{year.winner}</span> vs
+                      <span> {year.runnerup}</span>
+                    </Fragment>
+                  </ListGroup.Item>
+                );
+              })}
+            </ListGroup>
+          ) : (
+            <Fragment>
+              <h2>{year.name}</h2>
+              <span>{year.winner}</span> vs
+              <span>{year.runnerup}</span>
+            </Fragment>
+          )}
+        </Col>
       </Row>
     </Container>
   );
